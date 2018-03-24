e8e2904904b5baaf25c0b0a107e5f55dbc93d62c
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-23 20:24:08 -0400

    add input validation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -14,10 +14,10 @@ var config = {
     storageBucket: "train-schedule-55b13.appspot.com",
     messagingSenderId: "755536802556"
 };
+
 firebase.initializeApp(config);
 const database = firebase.database;
 const parentObject = database().ref().child('Trains');
-const childObjects = parentObject.child('Train');
 
 $(document).ready(function () {
 
@@ -27,46 +27,81 @@ function addRow(rowData) {
     var tableBody = $("#tablebody");
     var newRow = $("<tr>");
     $(newRow).addClass("lightestcontainer")
-    $(newRow).attr("data-index", rowData.index);
-    $(newRow).append(addColumn(rowData.trainName));
-    $(newRow).append(addColumn (rowData.destination));
-    $(newRow).append(addColumn(rowData.frequency));
-
+    $(newRow).attr("data-index", rowData.index)
+        .append(addColumn(rowData.trainName))
+        .append(addColumn(rowData.destination))
+        .append(addColumn(rowData.frequency));
     $(tablebody).append(newRow);
 }
 
 function addColumn(value) {
     var newCol = $("<th>");
-    $(newCol).attr("scope", "col");
-    $(newCol).text(value);
+    $(newCol).attr("scope", "col").text(value);
     return newCol;
 }
 
+function validData(data) {
+    returnValue = true;
+    var militaryTime = data.firstTrainTime;
+    var firstString = militaryTime.charAt(2);
+    var secondString = militaryTime.slice(0, 2);
+    var thirdString = militaryTime.slice(-2);
+    if (data.trainName == "") {
+        window.alert("You must have a name for the train");
+        $("#trainname").val("");
+        $("#trainname").focus();
+        returnValue = false;
+    } else if (data.destination == "") {
+        window.alert("You must have a train destination");
+        $("destination").val ("");
+        $("destination").focus();
+        returnValue = false;
+    } else if (isMilitaryTime(data.firstTrainTime) == false) {
+        window.alert("You must use valid military time format HH:mm");
+        $("#firsttraintime").val("");
+        $("#firsttraintime").focus();
+        returnValue = false;
+    } else if (isNaN(data.frequency) || data.frequency == "") {
+        window.alert("You must input train frequency (in minutes)");
+        $("#frequency").val("");
+        $("#frequency").focus();
+        returnValue = false;
+    }
 
+    return returnValue;
+}
+
+function isMilitaryTime(time) {
+    returnValue = true;
+    if (time.length !== 5) {
+        returnValue = false;
+    } else if (time.charAt(2) !== ":") {
+        returnValue = false;
+    } else if (isNaN(time.slice(0, 2))) {
+        returnValue = false;
+    } else if (isNaN(time.slice(-2))) {
+        returnValue = false;
+    }
+    return returnValue;
+}
 // Call backs
-$("#submitbutton").click ("click", function() {
+$("#submitbutton").click("click", function () {
     // TODO: need input validation
-    currentTrain.trainName = $("#trainname").val().trim();
-    currentTrain.destination = $("#destination").val().trim();
-    currentTrain.firstTrainTime = $("#firsttraintime").val().trim();
+    currentTrain.trainName = $("#trainname").val().trim()
+    currentTrain.destination = $("#destination").val().trim()
+    currentTrain.firstTrainTime = $("#firsttraintime").val().trim()
     currentTrain.frequency = $("#frequency").val().trim();
-    //TODO: send new values to database
-    // let newTrainRef = database.
-   parentObject.push(currentTrain)
-    // newTrainRef.setValue(obj.toDictionary())
-
+    if (validData(currentTrain)) {
+        parentObject.push(currentTrain)
+    }
 });
-
-// const parentObject = firebase.database().ref().child('Trains');
-
-
 
 parentObject.on("child_added", snapshot => {
     console.log(snapshot);
     currentTrain.index = snapshot.key
-    currentTrain.trainName = snapshot.val().trainName;
-    currentTrain.destination = snapshot.val().destination;
+    currentTrain.trainName = snapshot.val().trainName
+    currentTrain.destination = snapshot.val().destination
     currentTrain.firstTrainTime = snapshot.val().firstTrainTime
-    currentTrain.frequency = snapshot.val().frequency
+    currentTrain.frequency = snapshot.val().frequency;
     addRow(currentTrain);
 });
