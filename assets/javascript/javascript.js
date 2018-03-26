af884f90e117e9fed29dddb3d80bbd921a691ca2
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-25 21:51:39 -0400

    finalize base functionality

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,5 @@
-var currentTrain = {
-    index: "",
-    trainName: "",
-    destination: "",
-    firstTrainTime: "",
-    frequency: ""
-}
+var currentTrain = {index: "",trainName: "",
+    destination: "", firstTrainTime: "", frequency: ""}
 
 var config = {
     apiKey: "AIzaSyChFkhV6bhl9IMX-VXY5fkwGbg8_3Jts4s",
@@ -15,41 +10,50 @@ var config = {
     messagingSenderId: "755536802556"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(config); //creates connection to firebase
 const database = firebase.database;
 const parentObject = database().ref().child('Trains');
 
-$(document).ready(function () {
-
-});
-
-function addRow(rowData) {
+function addRow(rowData) { //retrieves data and adds data rows to display
+    var calcResults = nextArrival(rowData.firstTrainTime, rowData.frequency)
+    var nextTrainTime = moment(calcResults[0]).format("hh:mm a")
+    var minutesUntilArrival = calcResults[1]
     var tableBody = $("#tablebody");
     var newRow = $("<tr>");
     $(newRow).addClass("lightestcontainer")
     $(newRow).attr("data-index", rowData.index)
         .append(addColumn(rowData.trainName))
         .append(addColumn(rowData.destination))
-        .append(addColumn(rowData.frequency));
+        .append(addColumn(rowData.frequency))
+        .append(addColumn(nextTrainTime))
+        .append(addColumn(minutesUntilArrival))
     $(tablebody).append(newRow);
 }
 
-function addColumn(value) {
+function addColumn(value) { //called by addRow to populate cells
     var newCol = $("<th>");
     $(newCol).attr("scope", "col").text(value);
     return newCol;
 }
 
-function validData(data) {
+function nextArrival(firstTrain, frequency) { //does the time calculations
+    var nextTrainTime = moment(firstTrain, "HH:mm");
+    checkValue = -1
+    while (checkValue < 0) {
+        checkValue = moment(nextTrainTime).diff(moment(), "minutes");
+        if (checkValue < 0) {
+            nextTrainTime.add(Number(frequency), 'minutes');
+        }
+    }
+    var returnobject = [nextTrainTime, checkValue]
+    return returnobject;
+}
+
+function validData(data) { //checks input and provides user feedback
     returnValue = true;
-    var militaryTime = data.firstTrainTime;
-    var firstString = militaryTime.charAt(2);
-    var secondString = militaryTime.slice(0, 2);
-    var thirdString = militaryTime.slice(-2);
     if (data.trainName == "") {
         window.alert("You must have a name for the train");
         $("#trainname").val("").focus();
-        // $("#trainname").focus();
         returnValue = false;
     } else if (data.destination == "") {
         window.alert("You must have a train destination");
@@ -67,22 +71,23 @@ function validData(data) {
     return returnValue;
 }
 
-function isMilitaryTime(time) {
-    returnValue = true;
+function isMilitaryTime(time) { //called by validData to validate military time
+    var returnValue = true;
+    var hours = time.slice(0, 2)
+    var minutes = time.slice(-2)
     if (time.length !== 5) {
         returnValue = false;
     } else if (time.charAt(2) !== ":") {
         returnValue = false;
-    } else if (isNaN(time.slice(0, 2))) {
+    } else if (isNaN(hours) || hours > 24 || hours < 0 ) {
         returnValue = false;
-    } else if (isNaN(time.slice(-2))) {
+    } else if (isNaN(minutes) || minutes >= 60 || minutes < 0) {
         returnValue = false;
     }
     return returnValue;
 }
 // Call backs
 $("#submitbutton").click("click", function () {
-    // TODO: need input validation
     currentTrain.trainName = $("#trainname").val().trim()
     currentTrain.destination = $("#destination").val().trim()
     currentTrain.firstTrainTime = $("#firsttraintime").val().trim()
